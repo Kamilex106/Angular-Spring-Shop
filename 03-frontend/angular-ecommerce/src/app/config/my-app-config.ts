e8b7d28dd068bfc7a6deea47fcbc3147bb4294c4
Kamil Leleniewski <115708858+Kamilex106@users.noreply.github.com>
2025-04-03 18:17:41 +0200

    Add security

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oao3bjm5djLXMrbK5d7',
+    issuer: 'https://dev-96763280.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
